6831ce9cf9ce5901098900bd709f46c0dfcaec82
Mohit Keshri <sendtomohit99@gmail.com>
2025-05-01 20:18:01 +0530

    completed backend and frontend

---
+++ frontend/notes-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {BrowserRouter as Router , Routes , Route} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Signup from "./pages/signUp/SignUp";
+import Login from './pages/login/Login'
+import NotFound from "./pages/NotFound/NotFound"
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/><Route path="/signup" element={<Signup/>}/><Route path="/*" element={<NotFound/>}/>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App; 
